52b233b939a5fa45c2540b7b9f0c00a432f15832
Carlos Zansavio <carloshlzansavio@gmail.com>
2021-06-30 21:35:57 -0300

    Add typeorm with postgres as driver.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
+import "reflect-metadata";
 
-import express from 'express';
-import {router} from './router';
+import express from "express";
+import { router } from "./router";
+import "./database/connect";
 
 const app = express();
 
-app.get('/', (res, req) => req.send({ok: true}));
+app.get("/", (res, req) => req.send({ ok: true }));
 
 app.use(router);
 
-app.listen(process.env.PORT || 3004, () => console.log(`Server started on http://localhost:${process.env.PORT}`));
+app.listen(process.env.PORT || 3004, () =>
+  console.log(`Server started on http://localhost:${process.env.PORT}`)
+);
