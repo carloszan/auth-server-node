448d2035d75a206052d543f0847317b8bae5103f
Carlos Zansavio <carloshlzansavio@gmail.com>
2021-07-02 15:09:17 -0300

    Add auth middleware

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getRepository } from "typeorm";
 import { User } from "../database/entities/user";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 export const userRouter = Router();
 
-userRouter.get("/users", async (req, res) => {
+userRouter.get("/users", authMiddleware, async (req, res) => {
   const repository = getRepository(User);
 
   const users = await repository.query("select * from users");
